ecdfb8ac7750e73ef20a5653d718fb74b4058b8d
Sam Atkins <samatkins5@gmail.com>
2018-01-08 21:33:07 -0800

    feat: Iterate Deck titles

--- app/components/DecksView/DecksView.js
+++ app/components/DecksView/DecksView.js
@@ -1,9 +1,41 @@
+// @flow
+
 import React from 'react';
 import { Text, View } from 'react-native';
+
+const deckData = {
+  React: {
+    title: 'React',
+    questions: [
+      {
+        question: 'What is React?',
+        answer: 'A library for managing user interfaces',
+      },
+      {
+        question: 'Where do you make Ajax requests in React?',
+        answer: 'The componentDidMount lifecycle event',
+      },
+    ],
+  },
+  JavaScript: {
+    title: 'JavaScript',
+    questions: [
+      {
+        question: 'What is a closure?',
+        answer:
+          'The combination of a function and the lexical environment within which that function was declared.',
+      },
+    ],
+  },
+};
 
 const DecksView = () => (
   <View>
     <Text>Main decks view</Text>
+    {Object.keys(deckData).map((key) => {
+      const { title } = deckData[key];
+      return <Text key={title}>{title}</Text>;
+    })}
   </View>
 );
 
