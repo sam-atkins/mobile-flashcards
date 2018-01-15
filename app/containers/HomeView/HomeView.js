dc4d5c0b24b7e07f4b8b7fb83ed356ec63c08cc8
Sam Atkins <samatkins5@gmail.com>
2018-01-15 10:16:02 +0000

    feat: FlatList uses data from Redux store

--- app/containers/HomeView/HomeView.js
+++ app/containers/HomeView/HomeView.js
@@ -1,66 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FlatList, Text, TouchableOpacity, View } from 'react-native';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import WelcomeText from '../../components/WelcomeText';
 import { selectDeckTitleAndQuestions } from '../../selectors/deckSelectors';
 
-// TODO replace with `content` from Redux store via selector once set-up
-const hardCodedContent = [
-  {
-    title: 'React',
-    questions: [
-      {
-        question: 'What is React?',
-        answer: 'A library for managing user interfaces',
-      },
-      {
-        question: 'Where do you make Ajax requests in React?',
-        answer: 'The componentDidMount lifecycle event',
-      },
-    ],
-  },
-  {
-    title: 'JavaScript',
-    questions: [
-      {
-        question: 'What is a closure?',
-        answer:
-          'The combination of a function and the lexical environment within which that function was declared.',
-      },
-    ],
-  },
-];
+class HomeView extends Component {
+  render() {
+    this.renderItemList = ({ item }) => {
+      const { title, questions } = item;
+      return (
+        <TouchableOpacity
+          onPress={() =>
+            this.props.navigation.navigate('QuizOverview', {
+              title,
+              questions,
+            })
+          }
+        >
+          <Text>{title}</Text>
+          <Text>{questions.length} cards</Text>
+        </TouchableOpacity>
+      );
+    };
 
-const HomeView = (props) => {
-  const renderItemList = ({ item }) => {
-    const { title, questions } = item;
     return (
-      <TouchableOpacity
-        onPress={() =>
-          props.navigation.navigate('QuizOverview', {
-            title,
-            questions,
-          })
-        }
-      >
-        <Text>{title}</Text>
-        <Text>{questions.length} cards</Text>
-      </TouchableOpacity>
-    );
-  };
-
-  return (
-    <View>
-      <WelcomeText />
       <View>
-        <FlatList
-          data={hardCodedContent}
-          renderItem={renderItemList}
-          keyExtractor={item => item.title}
-        />
+        <WelcomeText />
+        <View>
+          <FlatList
+            data={this.props.content}
+            renderItem={this.renderItemList}
+            keyExtractor={item => item.title}
+          />
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
+}
+
+HomeView.propTypes = {
+  content: PropTypes.array.isRequired,
+  navigation: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = state => ({
