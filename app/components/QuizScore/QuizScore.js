976ea6b7e05c0c2c4691d0304b3a3a3b2ec1082c
Sam Atkins <samatkins5@gmail.com>
2018-01-17 10:42:27 +0000

    feat: Add reset quiz button

--- app/components/QuizScore/QuizScore.js
+++ app/components/QuizScore/QuizScore.js
@@ -2,22 +2,37 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import PrimaryButton from '../Buttons/PrimaryButton';
+import { resetQuiz } from '../../actions/quizActions';
 
-const QuizScore = ({ totalQuizQuestions, userQuizScore }) => (
+const QuizScore = ({ totalQuizQuestions, userQuizScore, userResetQuiz }) => (
   <View>
     <Text>
       Your scored {userQuizScore} points out of {totalQuizQuestions}.
     </Text>
+    <PrimaryButton
+      text="Reset Quiz?"
+      onPress={() => {
+        userResetQuiz();
+      }}
+    />
   </View>
 );
 
 QuizScore.propTypes = {
   totalQuizQuestions: PropTypes.number.isRequired,
   userQuizScore: PropTypes.number.isRequired,
+  userResetQuiz: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   userQuizScore: state.quiz.quizNumbers.quizScore,
 });
 
-export default connect(mapStateToProps)(QuizScore);
+const mapDispatchToProps = dispatch => ({
+  userResetQuiz: () => {
+    dispatch(resetQuiz());
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuizScore);
