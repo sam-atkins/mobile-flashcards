a2d7f793f0e1ddc67be4846443ccaba480901ba9
Sam Atkins <samatkins5@gmail.com>
2018-01-19 09:31:07 +0000

    feat: Add new card action creator and reducer

--- app/reducers/deckReducer.js
+++ app/reducers/deckReducer.js
@@ -1,8 +1,23 @@
 import { initialDeckData } from '../config/initState';
-import { ADD_NEW_DECK } from '../actions/deckActions';
+import { ADD_NEW_CARD, ADD_NEW_DECK } from '../actions/deckActions';
 
 const decks = (state = initialDeckData, action) => {
   switch (action.type) {
+    case ADD_NEW_CARD:
+      return {
+        ...state,
+        [action.payload.title]: {
+          title: state[action.payload.title].title,
+          quizLength: state[action.payload.title].quizLength + 1,
+          questions: [
+            ...state[action.payload.title].questions,
+            {
+              question: action.payload.question,
+              answer: action.payload.answer,
+            },
+          ],
+        },
+      };
     case ADD_NEW_DECK:
       return {
         ...state,
