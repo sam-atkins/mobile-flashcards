ebf17906a9a28ba470a8624bab91352cb87a3283
Sam Atkins <samatkins5@gmail.com>
2018-01-19 20:56:41 +0000

    feat: Add DatePicker to notification screen

--- app/containers/Notifications/Notifications.js
+++ app/containers/Notifications/Notifications.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { DatePickerIOS, Text, View } from 'react-native';
+import PrimaryButton from '../../components/Buttons/PrimaryButton';
 
 class Notifications extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = { chosenTime: new Date() };
+
+    this.setTime = this.setTime.bind(this);
+  }
+
+  setTime(newTime) {
+    this.setState({ chosenTime: newTime });
+  }
   render() {
     return (
       <View>
-        <Text>Notifications screen</Text>
+        <Text>
+          Get a daily reminder on those days when you haven't learned with the
+          app yet
+        </Text>
+        <DatePickerIOS
+          date={this.state.chosenTime}
+          onDateChange={this.setTime}
+          mode="time"
+        />
+        <PrimaryButton />
       </View>
     );
   }
