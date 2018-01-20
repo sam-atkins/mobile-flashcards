c57d7fec34206044ef8bf48d8e08ec94c81e0ee1
Sam Atkins <samatkins5@gmail.com>
2018-01-20 08:47:54 +0000

    style: Add styles to WelcomeText

--- app/components/WelcomeText/WelcomeText.js
+++ app/components/WelcomeText/WelcomeText.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import styled from 'styled-components/native';
+
+const Header = styled.Text`
+  font-size: 24;
+  text-align: center;
+  padding-top: 10;
+  padding-horizontal: 15;
+`;
+
+const SubHeader = Header.extend`
+  font-size: 18;
+`;
 
 const WelcomeText = () => (
   <View>
-    <Text>Welcome to Mobile Flashcards!</Text>
-    <Text>What do you want to learn today?</Text>
+    <Header>Welcome to Mobile Flashcards!</Header>
+    <SubHeader>What do you want to learn today?</SubHeader>
   </View>
 );
 
